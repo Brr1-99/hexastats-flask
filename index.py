6c975f6b50c2dca8288e38ecefa37fcbeedb5534
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2021-12-22 08:55:57 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
+from getData import getData
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    try:
+        # ?players=Bruno
+        players = request.args.get('players')
+        players = players.split(',')
+    except AttributeError:
+        players = []
+    gamers = getData(players)
+    # return render_template('home.html', data=gamers)
+    return gamers
