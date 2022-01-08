ffcb7c1d635dc582d751d179ed0e93ab576c2a7c
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-08 22:37:15 +0100

    update cors

--- index.py
+++ index.py
@@ -4,6 +4,13 @@ import json
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
 
 @app.route("/", methods=['GET'])
 def home():
